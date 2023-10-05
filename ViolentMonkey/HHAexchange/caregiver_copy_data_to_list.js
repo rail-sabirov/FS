ba7ca0ec24ac2ee6b13983bb5507a0f4d7156afc
Rail' Sabirov <rails@fivestarlhcsa.com>
2023-10-05 14:19:25 -0400

    HHA: Caregiver copy data for XLS list

---
+++ ViolentMonkey/HHAexchange/caregiver_copy_data_to_list.js
@@ -0,0 +1,38 @@
+try {
+    function dataForXLS() {
+      return $('#uxLblPAideCode').text() + String.fromCharCode(9)
+        + $('#ctl00_ContentPlaceHolder1_uxlblInfoName').text() + String.fromCharCode(9)
+        + $('#lblHireDate').text() + String.fromCharCode(9)
+        + $('#uxLblEmploymentType').text() + String.fromCharCode(9)
+        + ' ' + String.fromCharCode(9)
+        + $('#lblLastWorkDate').text() + String.fromCharCode(9)
+        + ' ' + String.fromCharCode(9)
+        + $('#ctl00_ContentPlaceHolder1_lblLangauges').text();
+  }
+
+  // Add button
+  $('<div class="fs-button-xls">Copy data for List</div>').insertBefore($('#uxLblMessage'));
+
+  $('.fs-button-xls').on('click', () => {
+    const obj = dataForXLS();
+
+    navigator.clipboard.writeText(JSON.stringify(obj));
+    alert('Caregiver Data is Copied!');
+  });
+
+
+
+  GM_addStyle(`
+    .fs-button-xls {
+        display: inline-block;
+        background-color: #447086;
+        border-radius: 4px;
+        color: white;
+        font-weight: 400;
+        padding: 2px 5px;
+        margin: 3px;
+        cursor: pointer;
+    }
+  `);
+
+} catch{}
